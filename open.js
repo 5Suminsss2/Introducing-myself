fe71b0fa463f7123f70acfe25e19382e48b40502
5Suminsss2 <1012ksm@naver.com>
2021-04-29 20:15:13 +0900

    Making "Open page"
    * Making a rotating image with transform and transition

---
+++ open.js
@@ -0,0 +1,7 @@
+const profile = document.querySelector('.profile_img');
+
+
+profile.addEventListener('click', () => {
+    profile.classList.toggle('active');
+    icons.classList.toggle('active');
+  });
