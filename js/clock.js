e072710e584dfa0ff36b727bcc38234029d602cb
5Suminsss2 <1012ksm@naver.com>
2021-05-14 20:50:31 +0900

    Modify "footer and clock"
    - Resizing footer
    - Resizing clock

--- js/clock.js
+++ js/clock.js
@@ -1,11 +1,3 @@
-
-//let today = new Date();   
-//document.write(today.toLocaleDateString() + '<br>');
-//document.write(today.toLocaleTimeString() + '<br>');
-//document.write(today.toLocaleString() + '<br><br>');
-
-//document.write(today.toLocaleDateString('en-US')); 
-
 function showClock(){
     var currentDate = new Date();
     var divClock = document.getElementById('divClock');
